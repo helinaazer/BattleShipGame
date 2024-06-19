d874ddedc20304b05adc4cbd8775f45615aa5e16
Helina Azer <hazer@uw.edu>
2024-06-19 14:55:17 -0700

    firstCommit

---
+++ script.js
@@ -0,0 +1,15 @@
+// // myScript.js
+// document.addEventListener("DOMContentLoaded", function () {
+//   const canvas = document.getElementById("test");
+//   const ctx = canvas.getContext("2d");
+
+//   // Set line properties
+//   ctx.strokeStyle = "blue"; // Line color
+//   ctx.lineWidth = 2; // Line width
+
+//   // Draw a line from (50, 50) to (350, 150)
+//   ctx.beginPath();
+//   ctx.moveTo(50, 50);
+//   ctx.lineTo(350, 150);
+//   ctx.stroke();
+// });
